de075a1c52602c020a721fdd3b6a09c4d2dcb780
carolcal <carolinecaleguer@gmail.com>
2025-07-31 10:41:48 -0300

    cpp08 finished

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -24,20 +24,30 @@
 int main()
 {
     try {
+        std::cout << PINK << "* TESTING VECTOR *" << RESET << std::endl;
         int arr1[] = {1, 2, 3};
+        std::cout <<"Creating vector v = {1, 2, 3}" << RESET << std::endl;
         std::vector<int> v(arr1, arr1 + 3);
-        std::cout << "vector: " << easyfind(v, 2) << std::endl;
+        std::cout << "Searching number 2 in vector, index = " << easyfind(v, 2) << std::endl;
 
+        std::cout << PINK << "\n* TESTING LIST *" << RESET << std::endl;
         int arr2[] = {4, 5, 6};
+        std::cout <<"Creating list l = {4, 5, 6}" << RESET << std::endl;
         std::list<int> l(arr2, arr2 + 3);
-        std::cout << "list: " << easyfind(l, 6) << std::endl;
+        std::cout << "Searching number 6 in list, index = " << easyfind(l, 6) << std::endl;
 
+        std::cout << PINK << "\n* TESTING DEQUE *" << RESET << std::endl;
         int arr3[] = {7, 8, 9};
+        std::cout <<"Creating deque d = {7, 8, 9}" << RESET << std::endl;
         std::deque<int> d(arr3, arr3 + 3);
-        std::cout << "deque: " << easyfind(d, 7) << std::endl;
+        std::cout << "Searching number 7 in deque, index =" << easyfind(d, 7) << std::endl;
+    
+        std::cout << PINK << "\n* THROWING ERROR *" << RESET << std::endl;
+        std::cout << "Searching number 6 in vector: " << easyfind(v, 7) << std::endl;
+
     }
     catch (const std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
     }
 
     return 0;
